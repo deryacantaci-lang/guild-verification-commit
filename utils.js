7e1c2fc8906ffc993400f0478cc0ece685c683a6
deryacantaci-lang <deryacantaci@gmail.com>
2026-03-15 01:23:37 +0300

    Add multi-language support

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:23:30.789Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-14T22:23:37.710Z
+Commit: Add multi-language support

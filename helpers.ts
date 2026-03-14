0a20296d065fa6030a2fb0eb1c4fa6c6a3f1220a
deryacantaci-lang <deryacantaci@gmail.com>
2026-03-15 01:25:23 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:25:16.919Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-14T22:25:23.475Z
+Commit: Resolve null pointer exception in user service

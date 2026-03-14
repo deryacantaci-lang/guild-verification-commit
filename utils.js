d86b99cb5a5401896b523f710c1b0595e1785e30
deryacantaci-lang <deryacantaci@gmail.com>
2026-03-15 00:50:03 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:48:33.867Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-14T21:50:03.573Z
+Commit: Resolve null pointer exception in user service

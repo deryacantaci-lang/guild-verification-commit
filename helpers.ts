dc30bed32e637e21dd7abf6835a6440275b62845
deryacantaci-lang <deryacantaci@gmail.com>
2026-03-15 00:49:59 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:49:57.532Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T21:49:59.728Z
+Commit: Fix race condition in async operations

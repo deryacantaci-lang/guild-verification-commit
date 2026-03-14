e11fa552727fbb9f2a53f176d6c8a360cd3eeb0f
deryacantaci-lang <deryacantaci@gmail.com>
2026-03-15 01:23:21 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:23:13.817Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T22:23:21.151Z
+Commit: Fix CSS styling bug on mobile devices

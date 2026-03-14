f40450a01745bf2682514078b292aac4edeb5b73
deryacantaci-lang <deryacantaci@gmail.com>
2026-03-15 01:22:39 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:50:03.573Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T22:22:39.792Z
+Commit: Fix CSS styling bug on mobile devices
